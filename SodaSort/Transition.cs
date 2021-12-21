b6e160c09682889df543f1ab6925a2cf7808ed7d
Sergey B <sergeyvbo@outlook.com>
2021-12-21 18:24:13 +0300

    refactoring

---
+++ SodaSort/Transition.cs
@@ -0,0 +1,17 @@
+﻿/*var data = new string[]
+{
+    "yryr",
+    "ryry",
+    "____",
+    "____"
+};*/
+
+
+
+
+
+
+
+
+// возвращает все успешные подпути
+public record Transition(string Path, State NextState, int Priority);
